ed6e22a9630ce11b65ec2edd7944300b96e86043
zhangmiaoping23 <645415776@qq.com>
2018-07-09 17:37:00 +0800

    基本框架 对应Burp Suit Professional v1.7.26

---
+++ DecryptExtender/src/burp/BurpExtender.java
@@ -0,0 +1,29 @@
+package burp;
+
+import java.io.PrintWriter;
+
+public class BurpExtender implements IBurpExtender {
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+        // set our extension name
+        this.callbacks = callbacks;
+        this.callbacks.setExtensionName("decrypt extension");
+
+        //在初始化的时候将burp设置成不拦截模式
+        this.callbacks.setProxyInterceptionEnabled(false);
+
+        helpers =  this.callbacks.getHelpers();
+
+        stdout = new PrintWriter(callbacks.getStdout(), true);
+        PrintWriter stderr = new PrintWriter(callbacks.getStderr(), true);
+
+        // write a message to our output stream
+        stdout.println("registerExtenderCallbacks Success ");
+
+    }
+
+    IExtensionHelpers helpers;
+    PrintWriter stdout;
+    IBurpExtenderCallbacks callbacks;
+}
